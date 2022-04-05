d300de01fc6d72f650b99bb82ec943caddd88cd6
SeaAustinCode <seaaustincode@gmail.com>
2022-04-05 14:11:46 -0700

    Items and Orders

---
+++ Order.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Order {
+    public String name;
+    public double total;
+    public boolean ready;
+    public ArrayList<Item>items = new ArrayList<Item>();
+    
+        // Order variables -- order1, order2 etc.
+    
+        // Application Simulations
+        // Use this example code to test various orders' updates
+    }
+
